bfe1c3971a7d9a8a30652bbb3ec9223deb1aef6e
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-11-10 10:15:25 +0300

    Добавьте файлы проекта.

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int x;
+    int y;
+    std::cout << "Type a number x";
+    std::cin >> x;
+    std::cout << "Type a number y";
+    std::cin >> y;
+
+}
