44baec2eaa21ab3247e0d42f856415e75dc3d672
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-11-13 15:10:35 +0300

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
-#include <string>
+#include <cmath>
+#include <vector>
+using namespace std;
 
 int main() {
-    int x;
-    int y;
-    std::cout << "Type a number x";
-    std::cin >> x;
-    std::cout << "Type a number y";
-    std::cin >> y;
-
+	int y = 0;
+	int k;
+	cin >> k;
+	int n;
+	cin >> n;
+	int m;
+	cin >> m;
+	vector <int>b(n);
+	vector <int>c(m);
+	for (int i = 0; i < b.size(); ++i) {
+		cin >> b[i];
+	}
+	for (int i = 0; i < c.size(); ++i) {
+		cin >> c[i];
+	}
+	b.insert(b.begin() + k, c.begin(), c.end());
+	for (int i = 0; i < b.size(); ++i) {
+		cout << b[i];
+	}
+	cout << b[k];
 }
