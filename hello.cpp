e1a5b1647a19f9dc7850497c463d2f3217b97675
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-11-14 11:31:00 +0300

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,27 +1,17 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <string>
 using namespace std;
 
+struct complexNumber {
+	int age;
+	string name;
+
+};
 int main() {
-	int y = 0;
-	int k;
-	cin >> k;
-	int n;
-	cin >> n;
-	int m;
-	cin >> m;
-	vector <int>b(n);
-	vector <int>c(m);
-	for (int i = 0; i < b.size(); ++i) {
-		cin >> b[i];
-	}
-	for (int i = 0; i < c.size(); ++i) {
-		cin >> c[i];
-	}
-	b.insert(b.begin() + k, c.begin(), c.end());
-	for (int i = 0; i < b.size(); ++i) {
-		cout << b[i];
-	}
-	cout << b[k];
+	complexNumber ted;
+	ted.age = 10;
+	ted.name = "Ted";
+	cout << "Name: " << ted.name <<"\n" << "Age: " << ted.age;
 }
