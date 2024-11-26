d88ef9c02f8ba32e6695482d8450824ee89b59e1
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2024-11-26 11:02:55 +0300

    summRY

--- hello.cpp
+++ hello.cpp
@@ -2,16 +2,32 @@
 #include <cmath>
 #include <vector>
 #include <string>
+#include <utility>
 using namespace std;
 
-struct complexNumber {
-	int age;
-	string name;
+int Sum(int a, int b ){
+	return a + b;
+}
+void hello() {
+	cout << "hello world";
+}
+enum class Color {
+	White,
+	Red,
+	Orange,
+	Blue,
+};
+struct Point {
+	double x = 0.0;
+	double y = 0.0;
+	double z = 0.0;
+	Color color;
 
 };
 int main() {
-	complexNumber ted;
-	ted.age = 10;
-	ted.name = "Ted";
-	cout << "Name: " << ted.name <<"\n" << "Age: " << ted.age;
+
+		tuple<string, int> p = { "hello", 42 };
+		auto [word, freq] = p;  // word = "hello"; freq = 42;
+		cout << word<<Sum(1,2);
+		hello();
 }
